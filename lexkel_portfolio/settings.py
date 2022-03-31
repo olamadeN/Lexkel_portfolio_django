83190aa5100d5e3f4d6a28a8204171663f0c49f6
nissi <olamademuyiwa@gmail.com>
2022-03-31 12:49:48 +0100

    hiding secret keys

--- lexkel_portfolio/settings.py
+++ lexkel_portfolio/settings.py
@@ -13,7 +13,8 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 from pathlib import Path
 import os
 import django_heroku
-import dj-database-url
+import dotenv
+import dj_database_url
 from decouple import config
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -23,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-8()7b)_4*$yul%3#j*@z0c8hb7z!^x98t2@u+r4nr3)&6s-jb9'
+
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
