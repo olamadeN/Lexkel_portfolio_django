9d1a3d00d91ac2103d43e4e2700fae39a9486a89
nissi <olamademuyiwa@gmail.com>
2022-04-05 15:17:39 +0100

    triang heroku

--- lexkel_portfolio/settings.py
+++ lexkel_portfolio/settings.py
@@ -25,11 +25,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 
 SECRET_KEY = config('SECRET_KEY')
-print (SECRET_KEY)
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1', '*']
+ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1', '*','https://lexkel-portfolio.herokuapp.com/']
 
 
 # Application definition
