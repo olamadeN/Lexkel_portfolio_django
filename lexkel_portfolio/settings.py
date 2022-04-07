9cd2f3c5308ef2266fd9a833bd897238b0e0c1e7
olamadeN <73506832+olamadeN@users.noreply.github.com>
2022-04-07 10:24:02 +0100

    Update settings.py

--- lexkel_portfolio/settings.py
+++ lexkel_portfolio/settings.py
@@ -30,6 +30,22 @@ DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com', '127.0.0.1']
 
+LOGGING = {
+    'version': 1,
+    'disable_existing_loggers': False,
+    'handlers': {
+        'console': {
+            'class': 'logging.StreamHandler',
+        },
+    },
+    'loggers': {
+        'django': {
+            'handlers': ['console'],
+             'level': os.getenv('DJANGO_LOG_LEVEL', 'DEBUG'),
+        },
+    },
+}
+
 
 # Application definition
 
